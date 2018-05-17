5958837851a3419f42cf6b7bcb88423bda8c780b
Kristian Larsen <kristian@greatcarrot.com>
2018-05-17 22:33:14 +0800

    added a component to component communication

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-Vue.component('box', {
+// 1. Global Event object created
+window.Event = new Vue();
+
+Vue.component('coupon', {
   template: `
-    
-  `
+    <input placeholder="Enter your coupon code" @blur="onCouponApplied">
+  `,
+  methods: {
+    onCouponApplied() {
+      // 2. local component emits custom event when trickered to global Event element
+      Event.$emit('applied');
+    }
+  }
 });
 
 let app = new Vue({
-  el: '#root'
+  el: '#root',
+  created() {
+    // 3. root element listening for event
+    Event.$on('applied', () => console.log('Handling it'))
+  }
 });
