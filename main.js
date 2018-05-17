85aa7b2a8893d8ed1d90b4b532cff160627c2de4
Kristian Larsen <kristian@greatcarrot.com>
2018-05-17 13:52:16 +0800

    added modal in html

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 Vue.component('message', {
-  props: ['title', 'body'],
+  props: [],
   data() {
     return {
-      isVisible: true
+      
     }
   },
   template: `
-    <article class="message" v-show="isVisible">
-      <div class="message-header">
-        <p>{{ title }}</p>
-        <button class="delete" aria-label="delete" @click="isVisible = false"></button>
-      </div>
-      <div class="message-body">
-        {{ body }}
-      </div>
-    </article>
+    
   `
 })
 
