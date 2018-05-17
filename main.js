a30055ab0d0e392bf568641777151d15bcc0b10a
Kristian Larsen <kristian@greatcarrot.com>
2018-05-17 22:23:42 +0800

    updated version number

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
-Vue.component('coupon', {
+Vue.component('box', {
   template: `
-    <input placeholder="Enter your coupon code" @blur="onCouponApplied">
-  `,
-  methods: {
-    onCouponApplied() {
-      // 1. local component emits custom event when trickered
-      this.$emit('applied');
-    }
-  }
+    
+  `
 });
 
 let app = new Vue({
-  el: '#root',
-  data: {
-    couponApplied: false
-  },
-  methods: {
-    onCouponApplied() {
-      // 3. root method changes state
-      this.couponApplied = true;
-    }
-  }
+  el: '#root'
 });
