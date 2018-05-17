aeaa24d3b57676221e30603cbe3a7ff50c18dfd0
Kristian Larsen <kristian@greatcarrot.com>
2018-05-17 22:52:50 +0800

    updated version number

--- main.js
+++ main.js
@@ -1,29 +1,6 @@
 Vue.component('modal', {
   template: `
-    <div class="modal is-active">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">
-            <slot name="header"></slot>
-          </p>
-          <button class="delete" aria-label="close"></button>
-        </header>
-        <section class="modal-card-body">
-          <slot>
-            Defaul;t content here.
-          </slot>
-        </section>
-        <footer class="modal-card-foot">
-          <button class="button is-success">
-            <slot name="footer-success"></slot>
-          </button>
-          <button class="button">
-            <slot name="footer-fail"></slot>
-          </button>
-        </footer>
-      </div>
-    </div>
+
   `
 });
 
