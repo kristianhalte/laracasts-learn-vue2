5d77594e36d5b1cc697d5ea4fc84db9abd9b56a7
Kristian Larsen <kristian@greatcarrot.com>
2018-05-17 10:58:06 +0800

    added hide modal method

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 Vue.component('message', {
   props: ['title', 'body'],
+  data() {
+    return {
+      isVisible: true
+    }
+  },
   template: `
-    <article class="message">
+    <article class="message" v-show="isVisible">
       <div class="message-header">
         <p>{{ title }}</p>
-        <button class="delete" aria-label="delete"></button>
+        <button class="delete" aria-label="delete" @click="hideModal"></button>
       </div>
       <div class="message-body">
         {{ body }}
       </div>
     </article>
-  `
+  `,
+  methods: {
+    hideModal() {
+      this.isVisible = false;
+    }
+  }
 })
 
-new Vue({
+let app = new Vue({
   el: '#root'
 })
