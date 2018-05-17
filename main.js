6f5ed5fb4ae887ba4eb430777465ea94a1841603
Kristian Larsen <kristian@greatcarrot.com>
2018-05-17 22:43:54 +0800

    first named slot

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ Vue.component('modal', {
       <div class="modal-background"></div>
       <div class="modal-card">
         <header class="modal-card-head">
-          <p class="modal-card-title">Modal title</p>
+          <p class="modal-card-title">
+            <slot name="header"></slot>
+          </p>
           <button class="delete" aria-label="close"></button>
         </header>
         <section class="modal-card-body">
-          <!-- Content ... -->
+          Some content
         </section>
         <footer class="modal-card-foot">
           <button class="button is-success">Save changes</button>
