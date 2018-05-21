f616b3b7d2ff31484b2546bd3e08343ab9d7e307
Kristian Larsen <kristian@greatcarrot.com>
2018-05-21 15:24:00 +0800

    updated version number

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app">
-    <counter></counter>
+    
   </div>
 </template>
 
 <script>
-import Counter from './components/Counter'
 export default {
   name: 'app',
-  components: { Counter },
+  components: {  },
   data () {
     return {
       
