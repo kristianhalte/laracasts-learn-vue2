3ccbb7527768a4cc9497d1326790c7596fbb30e2
Kristian Larsen <kristian@greatcarrot.com>
2018-05-21 15:43:05 +0800

    added two vue app with different data objects

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: '#one',
+  data: {
+    user: {
+      name: 'John Doe'
+    }
+  },
+  // render: h => h(App)
+})
+
+new Vue({
+  el: '#two',
+  data: {
+    user: {
+      name: 'Jane Doe'
+    }
+  },
+  // render: h => h(App)
 })
