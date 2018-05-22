a24265960b0f6a3d95a0f865eaefcfcb5b6e7aa8
Kristian Larsen <kristian@greatcarrot.com>
2018-05-23 07:23:37 +0800

    made a broken up coupon component

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 // import App from './App.vue'
 
+Vue.component('coupon', {
+  props: ['code'],
+  template: `
+    <input type="text" :value="code" @input="updateCode($event.target.value)" />
+  `,
+  methods: {
+    updateCode(code) {
+      // add validation
+      this.$emit('input', code);
+    }
+  }
+})
+
 new Vue({
   el: '#root',
   data: {
-    coupon: 'FREEBIE'
+    coupon: 'FREEBIE',
+    code: 'Other free stuff'
   }
   // render: h => h(App)
 })
