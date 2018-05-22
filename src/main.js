d71acc4ede550820be89cda6c40e0408e2027ea5
Kristian Larsen <kristian@greatcarrot.com>
2018-05-23 07:00:51 +0800

    updated version number

--- src/main.js
+++ src/main.js
@@ -1,26 +1,10 @@
 import Vue from 'vue'
 // import App from './App.vue'
 
-let store = {
-  user: {
-    name: 'John Doe'
-  }
-}
-
 new Vue({
-  el: '#one',
+  el: '#root',
   data: {
-    foo: 'bar',
-    shared: store
-  }
-  // render: h => h(App)
-})
 
-new Vue({
-  el: '#two',
-  data: {
-    foo: 'other bar',
-    shared: store
   }
   // render: h => h(App)
 })
