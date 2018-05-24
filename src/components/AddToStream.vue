0c61c215fbf2323a82a434b99ed335ec1e9a3375
Kristian Larsen <kristian@greatcarrot.com>
2018-05-24 20:52:38 +0800

    added ui for the form

--- src/components/AddToStream.vue
+++ src/components/AddToStream.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    add to stream
+    <div class="message">
+      <div class="message-header">
+        Push to the stream
+      </div>
+      <div class="message-body">
+        <form>
+          <p class="control">
+            <label class="label">Name</label>
+            <input class="input">
+            <label class="label">Status</label>
+            <textarea class="textarea"></textarea>
+          </p>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
